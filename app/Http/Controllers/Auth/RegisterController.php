9a8521ace09e018916de349f898e2706983fd96b
Tran Tuan Anh <yorkittran@gmail.com>
2020-02-27 16:23:07 +0700

    Remove dd RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,6 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        dd(constants('user.role.admin'));
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
